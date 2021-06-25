a7a0f701d5cbd87d6cccb0321fc5ebf75695cfde
rafi <rafiderberliner@googlemail.com>
2021-06-25 11:17:19 +0200

    Work In Progress Beam Search for CATR

--- avatar_models/captioning/catr/predict.py
+++ avatar_models/captioning/catr/predict.py
@@ -44,7 +44,7 @@ else:
 class CATRInference():
     def __init__(self):
         self.config = Config()
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', local_files_only=True)
         self.max_length = self.config.max_position_embeddings
         self.start_token = self.tokenizer.convert_tokens_to_ids(self.tokenizer._cls_token)
         self.end_token = self.tokenizer.convert_tokens_to_ids(self.tokenizer._sep_token)
@@ -78,17 +78,16 @@ class CATRInference():
         image = image.unsqueeze(0)
         beam_size = self.beam_size
         caption, cap_mask = self.create_caption_and_mask()
-        previous_log_prob = torch.zeros((beam_size, 1))
 
         if self.cuda_device.startswith("cuda"):
             image = image.cuda(self.cuda_device)
             caption = caption.cuda(self.cuda_device)
             cap_mask = cap_mask.cuda(self.cuda_device)
-            previous_log_prob = previous_log_prob.cuda(self.cuda_device)
+
         # model.eval()
 
         predictions = self.model(image, caption, cap_mask)
-        predictions = torch.nn.functional.log_softmax(predictions[:, 0, :])
+        predictions = predictions[:, 0, :]#torch.nn.functional.log_softmax(predictions[:, 0, :])
         previous_log_prob, candidate_indices = torch.topk(predictions, beam_size)
         preds = {i: np.zeros(self.max_length, dtype=int) for i in range(beam_size)}
         for i in range(beam_size):
@@ -103,7 +102,7 @@ class CATRInference():
         cap_mask[:, 0] = False
         for step in range(1, self.max_length - 1):
             predictions = self.model(image, caption, cap_mask)
-            predictions = torch.nn.functional.log_softmax(predictions[:, step, :])
+            predictions = predictions[:, step, :]
             candidates_log_prob, candidate_indices = torch.topk(predictions, beam_size)
             candidates_log_prob = torch.reshape(candidates_log_prob + previous_log_prob, (-1,))
             candidate_indices = torch.reshape(candidate_indices, (-1,))
@@ -148,6 +147,8 @@ class CATRInference():
                     #      i not in stop_idx])
                     caption = torch.index_select(caption, dim=0, index=left_idx)
                     cap_mask = torch.index_select(cap_mask, dim=0, index=left_idx)
+                    image = torch.index_select(image, dim=0, index=left_idx)
+                    previous_log_prob = torch.index_select(previous_log_prob, dim=0, index=left_idx)
                     # now that the finished sentences have been removed, we need to update the predictions dict accordingly
                     for i, key in enumerate(sorted(preds.keys())):
                         preds[i] = preds.pop(key)
@@ -166,6 +167,103 @@ class CATRInference():
         return output
 
     @torch.no_grad()
+    def infer_beam_2(self, image_path):
+
+        image = Image.open(image_path)
+        image = coco.val_transform(image)
+        image = image.unsqueeze(0)
+        beam_size = self.beam_size
+        caption, cap_mask = self.create_caption_and_mask()
+
+        if self.cuda_device.startswith("cuda"):
+            image = image.cuda(self.cuda_device)
+            caption = caption.cuda(self.cuda_device)
+            cap_mask = cap_mask.cuda(self.cuda_device)
+
+        # model.eval()
+
+        predictions = self.model(image, caption, cap_mask)
+        predictions = predictions[:, 0, :]#torch.nn.functional.log_softmax(predictions[:, 0, :])
+        previous_log_prob, candidate_indices = torch.topk(predictions, beam_size)
+        preds = {i: np.zeros(self.max_length, dtype=int) for i in range(beam_size)}
+        for i in range(beam_size):
+            preds[i][0] = candidate_indices[0][i]
+        # Copy entries a number of time equal to the beam size (the number of alternative paths)
+        # 1 means the dimensions stay untouched
+        image = image.repeat(beam_size, 1, 1, 1)
+        caption = caption.repeat(beam_size, 1)
+        cap_mask = cap_mask.repeat(beam_size, 1)
+        candidates = []
+        caption[:, 0] = candidate_indices
+        cap_mask[:, 0] = False
+        for step in range(1, self.max_length - 1):
+            predictions = self.model(image, caption, cap_mask)
+            predictions = predictions[:, step, :]
+            candidates_log_prob, candidate_indices = torch.topk(predictions, beam_size)
+            candidates_log_prob = torch.reshape(candidates_log_prob + previous_log_prob, (-1,))
+            candidate_indices = torch.reshape(candidate_indices, (-1,))
+            current_top_candidates, current_top_candidates_idx = torch.topk(candidates_log_prob, k=beam_size)
+
+            # Do the mapping best candidate and "source" of the best candidates
+            k_idx = torch.gather(candidate_indices, dim=0, index=current_top_candidates_idx)
+            prev_idx = torch.floor(current_top_candidates_idx / beam_size).to(torch.int32)
+
+            previous_log_prob = torch.unsqueeze(current_top_candidates, dim=1)
+            np_prev_idx = prev_idx.cpu().numpy()
+            # Overwrite the previous predictions due to the new best candidates
+            temp = caption.clone()
+            for i in range(prev_idx.shape[0]):
+                temp[i][:step] = caption[np_prev_idx[i]][:step]
+            caption = temp
+            preds = {i: preds[np_prev_idx[i]].copy() for i in range(prev_idx.shape[0])}
+
+            stop_idx = []
+            for i in range(k_idx.shape[0]):
+                preds[i][step] = k_idx[i]
+                if k_idx[i] == self.end_token:
+                    stop_idx.append(i)
+
+            # remove all finished captions and adjust all tensors accordingly...
+            if len(stop_idx):
+                for i in reversed(sorted(stop_idx)):
+                    candidate = preds.pop(i)
+                    loss = current_top_candidates[i]
+                    length = np.where(candidate == self.end_token)[0] + 1
+                    normalized_loss = loss / float(length)
+                    candidates.append((candidate, normalized_loss))
+                beam_size = beam_size - len(stop_idx)
+                if beam_size > 0:
+                    left_idx = torch.LongTensor([i for i in range(k_idx.shape[0]) if i not in stop_idx])
+                    k_idx = torch.LongTensor([k_idx[i] for i in range(k_idx.shape[0]) if i not in stop_idx])
+                    if self.cuda_device.startswith("cuda"):
+                        left_idx = left_idx.cuda(self.cuda_device)
+                        k_idx = k_idx.cuda(self.cuda_device)
+                    # current_top_candidates = torch.IntTensor(
+                    #     [current_top_candidates[i] for i in range(current_top_candidates.shape[0]) if
+                    #      i not in stop_idx])
+                    caption = torch.index_select(caption, dim=0, index=left_idx)
+                    cap_mask = torch.index_select(cap_mask, dim=0, index=left_idx)
+                    image = torch.index_select(image, dim=0, index=left_idx)
+                    previous_log_prob = torch.index_select(previous_log_prob, dim=0, index=left_idx)
+                    # now that the finished sentences have been removed, we need to update the predictions dict accordingly
+                    for i, key in enumerate(sorted(preds.keys())):
+                        preds[i] = preds.pop(key)
+                else:
+                    break  # No sequences unfinished
+
+            # predicted_id = torch.argmax(predictions, axis=-1)
+            pass
+            # if predicted_id[0] == self.end_token:
+            #     #return caption
+            #     break
+            caption[:, step] = k_idx
+            cap_mask[:, step] = False
+
+        output = self.tokenizer.decode(caption[0].tolist(), skip_special_tokens=True)
+        return output
+
+
+    @torch.no_grad()
     def infer(self, image_path):
 
         image = Image.open(image_path)
@@ -178,12 +276,12 @@ class CATRInference():
             caption = caption.cuda(self.cuda_device)
             cap_mask = cap_mask.cuda(self.cuda_device)
         #model.eval()
-        for i in range(self.config.max_position_embeddings - 1):
+        for i in range(self.max_length - 1):
             predictions = self.model(image, caption, cap_mask)
             predictions = predictions[:, i, :]
             predicted_id = torch.argmax(predictions, axis=-1)
 
-            if predicted_id[0] == 102:
+            if predicted_id[0] == self.end_token:
                 #return caption
                 break
             caption[:, i+1] = predicted_id[0]
@@ -195,7 +293,7 @@ class CATRInference():
 if __name__ == "__main__":
     image_path = "/home/rafi/_datasets/ADE20K/images/training/u/utility_room/ADE_train_00019432.jpg"
     catr = CATRInference()
-    output = catr.infer_beam(image_path)
+    output = catr.infer(image_path)
     #result = catr.tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
     #result = tokenizer.decode(output[0], skip_special_tokens=True)
     print(output)

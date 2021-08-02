d7664dbbd8a1eae59598b7351b6717da1c179674
rafi <rafiderberliner@googlemail.com>
2021-08-02 14:38:42 +0200

    Make the question/answer creation with transformer work

--- avatar_models/vqa/create_qa_pairs.py
+++ avatar_models/vqa/create_qa_pairs.py
@@ -1,11 +1,14 @@
-# you need to clone https://github.com/patil-suraj/question_generation in the directory of the script
+# Code reusing snippets from https://github.com/patil-suraj/question_generation
+# which itself uses transformers from hugging face for question generation
 
 
 import json
 import nltk
-from question_generation.pipelines import pipeline
+from avatar_models.vqa.pipelines import pipeline
 from avatar_models.utils.util import get_config
-nltk.download('punkt')
+import jsonlines
+import os
+
 
 def preprocess_qa(qa):
     '''This monstrosity of a function tries to clean the super messy QA pairs.'''
@@ -39,7 +42,9 @@ def preprocess_qa(qa):
         q = q.replace(" we can see what", "")
         q = q.lower()
         q = "What is " + q
-    clicked = ["Where is the picture clicked?","Where is the image clicked?","Where is the image clicked on?","Where is this picture clicked?","Where does the picture appear to be clicked?","What is the image clicked in?"]
+    clicked = ["Where is the picture clicked?", "Where is the image clicked?", "Where is the image clicked on?",
+               "Where is this picture clicked?", "Where does the picture appear to be clicked?",
+               "What is the image clicked in?"]
     if q in clicked:
         q = "Is this indoors or outdoors?"
     q = q.replace("In front , we see what?", "What is in the front?")
@@ -52,15 +57,16 @@ def preprocess_qa(qa):
     a = a.replace("3", "three")
     a = a.replace("4", "four")
     if a == "outdoors" or a == "indoors":
-        q = "Is this outdoors on indoors?"
-    if a  in ["red", "blue", "green", "yellow", "white", "black", "purple", "orange", "grey"]:
+        q = "Is this outdoors or indoors?"
+    if a in ["red", "blue", "green", "yellow", "white", "black", "purple", "orange", "grey"]:
         q = q.replace("What kind of", "What color")
         q = q.replace("What type of", "What color")
         q = q.replace("What is the name of", "What is the color of")
         q = q.replace("What is", "What color is")
         q = q.replace("type", "color")
 
-    return q,a
+    return q, a
+
 
 def get_multiple_answers(answer):
     '''Splits an answer into multiple answers for questions such as the following:
@@ -78,65 +84,78 @@ def get_multiple_answers(answer):
     answers = [a.strip() for a in answers]
     return answers
 
-# a list of weird questions that are often repeated — these are discarded from the final qa set
-shit_list=["What is the name of the building in front of the building?",
-          "What can we see ?",
-          "What types of chairs are there?",
-          "What are the three people ?",
-          "What is in the background ?",
-          "What types of trees are on the roller coaster?",
-          "What are railings on the left side ?",
-          "What are chairs, cupboards, lights and bottles?",
-          "What are trees, light poles, buildings, vehicles on the road?"]
 
-conf = get_config()
-LOCALIZED_NARRATIVES_FILE = conf["ade20k_localized_narratives_train_file"]
+if __name__ == "__main__":
 
-# create the pipeline
-nlp = pipeline("question-generation")
+    nltk.download('punkt')
 
-# open ade20k localized narratives train captions
-file = open(LOCALIZED_NARRATIVES_FILE, "r").readlines()
-print("Successfully loaded the ADE20K localized narratives training data set.")
+    # a list of weird questions that are often repeated — these are discarded from the final qa set
+    shit_list = ["What is the name of the building in front of the building?",
+                 "What can we see ?",
+                 "What types of chairs are there?",
+                 "What are the three people ?",
+                 "What is in the background ?",
+                 "What types of trees are on the roller coaster?",
+                 "What are railings on the left side ?",
+                 "What are chairs, cupboards, lights and bottles?",
+                 "What are trees, light poles, buildings, vehicles on the road?"]
 
-# create qa pairs
-qa = {}
-for ann in annotations:
-    d = []
-    try:
-        qa_pairs = nlp(ann['caption'])
-        for pair in qa_pairs:
-            dict_ = {}
-            dict_['question'] = pair['question']
-            dict_['answer'] = pair['answer']
-            d.append(dict_)
-        qa[ann['image_id']] = d
-    except:
-        pass
+    conf = get_config()
+    LOCALIZED_NARRATIVES_FILE = conf["ade20k_localized_narratives_train_file"]
+    ADE20K_DIR = conf["ade20k_dir"]
+    # create the pipeline
+    nlp = pipeline("question-generation")
 
-# preprocess/clean the qa pairs
-for image in qa:
-    for qa_pair in qa[image]:
-        q,a = preprocess_qa(qa_pair)
-        qa_pair['question'] = q
-        qa_pair['answer'] = a
+    # open ade20k localized narratives train captions
+    with jsonlines.open(LOCALIZED_NARRATIVES_FILE) as reader:
+        annotations = [i for i in iter(reader)]
 
-cleaned_qa = {}
-for image in qa:
-    cleaned_qa[image] = []
-    for qa_pair in qa[image]:
-        question = qa_pair['question']
-        answer = qa_pair['answer']
+    # annotations = open(LOCALIZED_NARRATIVES_FILE, "r").readlines()
+    print("Successfully loaded the ADE20K localized narratives training data set.")
 
-        if "," in answer: # answers with multiple choices
-            answers = get_multiple_answers(answer)
-            for ans in answers:
-                cleaned_qa[image].append({"question":question, "answer":ans})
+    # create qa pairs
+    qa = {}
+    for i, ann in enumerate(annotations):
+        d = []
+        try:
+            qa_pairs = nlp(ann['caption'])
+            for pair in qa_pairs:
+                dict_ = {}
+                dict_['question'] = pair['question']
+                dict_['answer'] = pair['answer']
+                d.append(dict_)
+            qa[ann['image_id']] = d
 
-        elif answer not in question:
-            cleaned_qa[image].append(qa_pair)
+        except:
+            pass
 
-# save to disk
-with open('ade20k_qa_cleaned.json', 'w') as fp:
-    json.dump(nothing, fp)
-print("Saved the dataset to disk.")
+        if i == 1:
+            break
+
+    # preprocess/clean the qa pairs
+    for image in qa:
+        for qa_pair in qa[image]:
+            q, a = preprocess_qa(qa_pair)
+            qa_pair['question'] = q
+            qa_pair['answer'] = a
+
+    cleaned_qa = {}
+    for image in qa:
+        cleaned_qa[image] = []
+        for qa_pair in qa[image]:
+            question = qa_pair['question']
+            answer = qa_pair['answer']
+
+            if "," in answer:  # answers with multiple choices
+                answers = get_multiple_answers(answer)
+                for ans in answers:
+                    cleaned_qa[image].append({"question": question, "answer": ans})
+
+            elif answer not in question:
+                cleaned_qa[image].append(qa_pair)
+
+    # save to disk
+    file_to_save = os.path.join(ADE20K_DIR, 'ade20k_qa_cleaned.json')
+    with open(file_to_save, 'w') as fp:
+        json.dump(cleaned_qa, fp)
+    print(f"Saved the dataset to disk: {file_to_save}")

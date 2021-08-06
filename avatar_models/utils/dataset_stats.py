59efc32e089af42d7ab23699f72d4906007286ae
rafi <rafiderberliner@googlemail.com>
2021-08-06 12:02:03 +0200

    Adding Some Stats.

--- avatar_models/utils/dataset_stats.py
+++ avatar_models/utils/dataset_stats.py
@@ -1,4 +1,5 @@
-from avatar_models.utils.util import load_preprocessed_vqa_data, get_ade20_vqa_data, get_config
+from avatar_models.utils.util import load_preprocessed_vqa_data,  get_config
+from avatar_models.utils.bleu import get_ade20k_caption_annotations
 import os, json
 
 if __name__ == "__main__":
@@ -8,11 +9,6 @@ if __name__ == "__main__":
     print(f"VQA 2.0 Training Size: {len(X_train)}")
     print(f"VQA 2.0 Testing Size: {len(X_val)}")
 
-    # vqa_ade20k_train = get_ade20_vqa_data("merged_synthetic_vqa_train.jsonl")
-    # vqa_ade20k_test = get_ade20_vqa_data("merged_synthetic_vqa_test.jsonl")
-
-
-
     ADE20K_SYNTHETIC_DATASET = os.path.join(config["ade20k_vqa_dir"], config["ade20k_vqa_file_train_test"])
     with open(ADE20K_SYNTHETIC_DATASET, "r") as f:
         vqa_ade20k_dataset = json.load(f)
@@ -20,3 +16,6 @@ if __name__ == "__main__":
 
     print(f"ADE20K VQA Training Size: {len(vqa_ade20k_dataset['training']['question'])}")
     print(f"ADE20K  Testing Size: {len(vqa_ade20k_dataset['testing']['question'])}")
+
+    ade20k_captions = get_ade20k_caption_annotations()
+    print(f"ADE20K number of images with 2 captions: {len(ade20k_captions)}")

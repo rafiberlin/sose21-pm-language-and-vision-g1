a3e86e2f09b1c5b7d8d4555b116c2dfb5880c40c
rafi <rafiderberliner@googlemail.com>
2021-08-06 12:17:25 +0200

    Adding Some Stats.

--- avatar_models/utils/dataset_stats.py
+++ avatar_models/utils/dataset_stats.py
@@ -19,3 +19,13 @@ if __name__ == "__main__":
 
     ade20k_captions = get_ade20k_caption_annotations()
     print(f"ADE20K number of images with 2 captions: {len(ade20k_captions)}")
+    caption_lengths_old = []
+    caption_lengths_new = []
+    for image in ade20k_captions:
+        caption_lengths_old.append(len(ade20k_captions[image][0].split()))
+        caption_lengths_new.extend([len(caption.split()) for caption in ade20k_captions[image]])
+
+    average_caption_length_new = sum(caption_lengths_new) / len(caption_lengths_new)
+    average_caption_length_old = sum(caption_lengths_old) / len(caption_lengths_old)
+    print(f"ADE20K average caption length with our captions: {average_caption_length_new}")
+    print(f"ADE20K average caption length originally: {average_caption_length_old}")

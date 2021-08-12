a24b98b9ba26c4f3c74d5f24b487119d94b22847
rafi <rafiderberliner@googlemail.com>
2021-08-12 09:55:21 +0200

    Adding Some Stats.

--- avatar_models/utils/dataset_stats.py
+++ avatar_models/utils/dataset_stats.py
@@ -6,8 +6,11 @@ if __name__ == "__main__":
 
     X_train, X_val, _, _, _, _ = load_preprocessed_vqa_data()
     config = get_config()
-    print(f"VQA 2.0 Training Size: {len(X_train)}")
-    print(f"VQA 2.0 Testing Size: {len(X_val)}")
+    print(f"VQA 2.0 Custom Training Size: {len(X_train)}")
+    print(f"VQA 2.0 Custom Testing Size: {len(X_val)}")
+    print(f"VQA 2.0 Custom Training Num Images: {len(set(X_train['image_id']))}")
+    print(f"VQA 2.0 Custom Testing Num Images: {len(set(X_val['image_id']))}")
+
 
     ADE20K_SYNTHETIC_DATASET = os.path.join(config["ade20k_vqa_dir"], config["ade20k_vqa_file_train_test"])
     with open(ADE20K_SYNTHETIC_DATASET, "r") as f:
@@ -15,7 +18,9 @@ if __name__ == "__main__":
 
 
     print(f"ADE20K VQA Training Size: {len(vqa_ade20k_dataset['training']['question'])}")
+    print(f"ADE20K VQA Training Number Images: {len(set(vqa_ade20k_dataset['training']['image_path'].values()))}")
     print(f"ADE20K  Testing Size: {len(vqa_ade20k_dataset['testing']['question'])}")
+    print(f"ADE20K VQA Testing Number Images: {len(set(vqa_ade20k_dataset['testing']['image_path'].values()))}")
 
     ade20k_captions = get_ade20k_caption_annotations()
     print(f"ADE20K number of images with 2 captions: {len(ade20k_captions)}")
